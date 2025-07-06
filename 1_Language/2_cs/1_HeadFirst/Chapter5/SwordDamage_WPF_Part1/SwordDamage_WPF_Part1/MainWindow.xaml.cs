e4e07821ead11bfdf49f4e9cac443d2e3b23883c
binna <every5116@naver.com>
2025-07-07 08:59:46 +0900

    헤드퍼스트 C#
    - Chpater5

---
+++ 1_Language/2_cs/1_HeadFirst/Chapter5/SwordDamage_WPF_Part1/SwordDamage_WPF_Part1/MainWindow.xaml.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Windows;
+
+namespace SwordDamage_WPF_Part1
+{
+    public partial class MainWindow : Window
+    {
+        Random random = new Random();
+        SwordDamage swordDamage = new SwordDamage();
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            swordDamage.SetMagic(false);
+            swordDamage.SetFlaming(false);
+
+            RollDice();
+        }
+
+        public void RollDice()
+        {
+            swordDamage.SetRoll(random.Next(1, 7) + random.Next(1, 7) + random.Next(1, 7));
+            DisplayDamage();
+        }
+
+        private void DisplayDamage()
+        {
+            damage.Text = swordDamage.GetDisplayDamageText();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            RollDice();
+        }
+
+        private void Flaming_Checked(object sender, RoutedEventArgs e)
+        {
+            swordDamage.SetFlaming(true);
+            DisplayDamage();
+        }
+
+        private void Flaming_Unchecked(object sender, RoutedEventArgs e)
+        {
+            swordDamage.SetFlaming(false);
+            DisplayDamage();
+        }
+
+        private void Magic_Checked(object sender, RoutedEventArgs e)
+        {
+            swordDamage.SetMagic(true);
+            DisplayDamage();
+        }
+
+        private void Magic_Unchecked(object sender, RoutedEventArgs e)
+        {
+            swordDamage.SetMagic(false);
+            DisplayDamage();
+        }
+    }
+}
---
+++ 1_Language/2_cs/1_HeadFirst/Chapter5/SwordDamage_WPF_Part1/SwordDamage_WPF_Part1/SwordDamage.cs
@@ -0,0 +1,61 @@
+﻿using System.Diagnostics;
+
+namespace SwordDamage_WPF_Part1
+{
+    internal class SwordDamage
+    {
+        public const int BASE_DAMAGE = 3;
+        public const int FLAME_DAMAGE = 2;
+
+        private int Roll;
+
+        private decimal MagicMultiplier = 1m;
+
+        private int FlamingDamage = 0;
+        private int Damage;
+
+        public void SetRoll(int newRoll)
+        {
+            Roll = newRoll;
+        }
+
+        public string GetDisplayDamageText()
+        {
+            return "Rolled " + Roll + " for " + Damage + " HP";
+        }
+
+        public void SetMagic(bool isMagic)
+        {
+            if (isMagic)
+            {
+                MagicMultiplier = 1.75M;
+            }
+            else
+            {
+                MagicMultiplier = 1M;
+            }
+
+            CalculateDamage();
+
+            Debug.WriteLine($"2. CalculateDamage finished : {Damage} (roll : {Roll})");
+        }
+
+        public void SetFlaming(bool isFlaming)
+        {
+            CalculateDamage();
+
+            if (isFlaming)
+            {
+                Damage += FLAME_DAMAGE;
+            }
+
+            Debug.WriteLine($"3. CalculateDamage finished : {Damage} (roll : {Roll})");
+        }
+
+        private void CalculateDamage()
+        {
+            Damage = (int)(Roll * MagicMultiplier) + BASE_DAMAGE + FlamingDamage;
+            Debug.WriteLine($"1. CalculateDamage finished : {Damage} (roll : {Roll})");
+        }
+    }
+}
